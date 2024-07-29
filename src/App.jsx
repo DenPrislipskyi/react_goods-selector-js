a3f13349b1d2afb34848c76f6880072df2fd5c21
pryslipskyi.d <denys.pryslipskyi@nixs.com>
2024-07-29 10:57:46 +0200

    refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
+import classNames from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
-import { useState } from 'react';
 
-export const goods = [
+export const goodsFromServer = [
   'Dumplings',
   'Carrot',
   'Eggs',
@@ -16,65 +17,70 @@ export const goods = [
 ];
 
 export const App = () => {
-  const [value, setValue] = useState('Jam');
+  const [goods, setGoods] = useState('Jam');
+
+  const handleClearGoods = () => {
+    setGoods('');
+  };
 
   return (
     <main className="section container">
       <h1 className="title is-flex is-align-items-center">
-        {value === '' ? 'No goods selected' : `${value} is selected`}
-        {value === '' ? (
-          ''
-        ) : (
+        {goods ? `${goods} is selected` : 'No goods selected'}
+        {goods && (
           <button
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => {
-              setValue('');
-            }}
+            onClick={handleClearGoods}
           />
         )}
       </h1>
 
       <table className="table">
         <tbody>
-          {goods.map(good => (
-            <tr
-              key={good}
-              data-cy="Good"
-              className={value === good ? 'has-background-success-light' : ''}
-            >
-              <td>
-                {value === good ? (
-                  <button
-                    data-cy="RemoveButton"
-                    type="button"
-                    className="button is-info"
-                    onClick={() => {
-                      setValue('');
-                    }}
-                  >
-                    -
-                  </button>
-                ) : (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={() => {
-                      setValue(good);
-                    }}
-                  >
-                    +
-                  </button>
-                )}
-              </td>
+          {goodsFromServer.map(good => {
+            const isSelected = goods === good;
 
-              <td data-cy="GoodTitle" className="is-vcentered">
-                {good}
-              </td>
-            </tr>
-          ))}
+            return (
+              <tr
+                key={good}
+                data-cy="Good"
+                className={classNames({
+                  'has-background-success-light': isSelected,
+                })}
+              >
+                <td>
+                  {isSelected && (
+                    <button
+                      data-cy="RemoveButton"
+                      type="button"
+                      className="button is-info"
+                      onClick={handleClearGoods}
+                    >
+                      -
+                    </button>
+                  )}
+                  {!isSelected && (
+                    <button
+                      data-cy="AddButton"
+                      type="button"
+                      className="button"
+                      onClick={() => {
+                        setGoods(good);
+                      }}
+                    >
+                      +
+                    </button>
+                  )}
+                </td>
+
+                <td data-cy="GoodTitle" className="is-vcentered">
+                  {good}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </main>
